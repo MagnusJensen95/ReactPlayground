b123be733dd35f9c24ee180456ef224062d84c4c
Magnus Jensen <mag@it-minds.dk>
2018-11-01 13:34:55 +0100

    needs editing, addition, filter and title

--- to-do-react/src/components/ToDoList/ListItem/ListItem.js
+++ to-do-react/src/components/ToDoList/ListItem/ListItem.js
@@ -1,8 +1,100 @@
 import React, { Component } from "react";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import "./ListItem.css";
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-export default class ListItem extends Component {
-  state = {};
+const SavedListItem = (props) => {
+
+  return (<>
+    <ListItemText primary={props.title} />
+    <ListItemSecondaryAction className="checkbox-margin">
+      <Checkbox
+        onChange={props.onCheck}
+        checked={props.checked}
+
+      />
+      <Button mini variant="fab" color="secondary" aria-label="Edit" onClick={props.onEdit}>
+        <Icon fontSize="small">edit_icon</Icon>
+      </Button>
+    </ListItemSecondaryAction>
+  </>)
+}
+
+const EditableListItem = (props) => {
+  let val = props.title;
+  return (<>
+    <TextField defaultValue={props.title} />
+    <ListItemSecondaryAction className="checkbox-margin">
+      <Checkbox
+        onChange={props.onCheck}
+        checked={props.checked}
+
+      />
+      <Button mini variant="fab" color="secondary" aria-label="Edit" onClick={() => props.onSave(props.title)}>
+        <Icon fontSize="small">check</Icon>
+      </Button>
+    </ListItemSecondaryAction>
+  </>)
+}
+
+export default class ListItemCustom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+      editing: false,
+
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleSaveClick = this.handleSaveClick.bind(this);
+  }
+
+  handleToggle = () => {
+    this.setState(state => ({
+      checked: !state.checked
+    }));
+
+  }
+
+  handleEditClick = () => {
+    this.setState(() => ({
+      editing: true
+    }));
+
+  }
+
+  handleSaveClick = (newTitle) => {
+    console.log(newTitle);
+
+
+    this.props.updateTitle(this.props.index, newTitle)
+    this.setState(() => ({
+      editing: false
+    }));
+
+  }
+
+
+
   render() {
-    return <div>{this.props.item}</div>;
+    return <ListItem className="list-item">
+      {this.state.editing ?
+
+
+        <EditableListItem title={this.props.item.title} onSave={(newTitle) => this.handleSaveClick(newTitle)} onCheck={this.handleToggle} />
+
+        :
+
+        <SavedListItem title={this.props.item.title} onEdit={this.handleEditClick} onCheck={this.handleToggle} />
+
+      }
+
+    </ListItem>;
   }
 }
--- to-do-react/src/components/ToDoList/ToDoList.js
+++ to-do-react/src/components/ToDoList/ToDoList.js
@@ -1,26 +1,63 @@
 import React, { Component } from "react";
 import List from "@material-ui/core/List";
-import ListItem from "./ListItem";
+import ListItemCustom from "./ListItem";
+import './ToDoList.css';
+import Divider from '@material-ui/core/Divider';
 
 export default class ToDoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       listItems: [
-        "Do the dishes",
-        "Cook Dinner",
-        "Become React Developer",
-        "Learn proper javascript"
+        {
+          title: "Do the dishes",
+          completed: false
+        },
+        {
+          title: "Cook Dinner",
+          completed: false
+        },
+        {
+          title: "Become React Developer",
+          completed: false
+        },
+        {
+          title: "Learn proper javascript",
+          completed: false
+        }
+
+
       ],
       nextToDo: ""
     };
   }
+
+  changeListItem = (index, newTitle) => {
+    let listItemsUpdated = this.state.listItems;
+
+    console.log(index);
+    console.log(listItemsUpdated);
+
+    listItemsUpdated[index].title = newTitle;
+    this.setState((state) => ({
+      listItems: listItemsUpdated
+    }));
+  }
+
+
   render() {
     const items = this.state.listItems;
-    const listItems = items.map((elm, idx) => (
-      <ListItem item={elm} key={idx} />
-    ));
+    const listItems = items.map((elm, idx) => {
 
-    return <List>{listItems}</List>;
+      return (
+        <div key={idx}>
+          <ListItemCustom item={elm} index={idx} updateTitle={this.changeListItem} />
+          {idx < items.length - 1 ? <Divider /> : <></>}
+
+        </div>
+      )
+    });
+
+    return <List className="list-main-container">{listItems}</List>;
   }
 }
